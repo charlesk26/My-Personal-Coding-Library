004e57ea0cbfdb5a750a16fc2084e3b0096a2f71
charleskolozsvary <charleskolozsvary@gmail.com>
2020-01-25 22:38:01 -0500

    latest stuff/ everything turned in

--- Algorithms and Data Structures quarter 2/src/com/jetbrains/TestingClass.java
+++ Algorithms and Data Structures quarter 2/src/com/jetbrains/TestingClass.java
@@ -2,8 +2,13 @@ package com.jetbrains;
 
 public class TestingClass
 {
+    static double getVel(int work)
+    {
+        return Math.sqrt(2*work/300);
+    }
     public static void main(String[] args)
     {
+
         int n = 11;
         int count1 = 0;
         int count2 = 0;
@@ -24,7 +29,11 @@ public class TestingClass
             }
 
         }
-        System.out.println(count1+" "+count2+" "+count3);
-        System.out.println((n*(n-1)*(n-2))/6);
+        //System.out.println(count1+" "+count2+" "+count3);
+        //System.out.println((n*(n-1)*(n-2))/6);
+        for(int i = 1000; i<17000; i+= 1000)
+        {
+            System.out.println(getVel(i));
+        }
     }
 }
